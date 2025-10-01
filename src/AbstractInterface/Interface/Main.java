03f3a61e77e37044e0e45f121791cdc794f5fab8
Vishal <vnaik9900@gmail.com>
2025-10-01 22:41:25 +0530

    Updated Interface code

--- src/AbstractInterface/Interface/Main.java
+++ src/AbstractInterface/Interface/Main.java
@@ -2,23 +2,28 @@ package AbstractInterface.Interface;
 
 public class Main {
     public static void main(String[] args) {
-        Camera cam = new Camera();
-        MP3Player play = new MP3Player();
-        smartPhone sp = new smartPhone();
+        // Camera cam = new Camera();
+        // MP3Player play = new MP3Player();
+        // smartPhone sp = new smartPhone();
 
-        cam.TakePhoto();
-        play.PlayMusic();
-        sp.PlayMusic();
-        sp.TakePhoto();
-        // Gadgets[] functions = {
-        // new Camera(),
-        // new MP3Player(),
-        // new smartPhone()
-        // };
+        // cam.TakePhoto();
+        // play.PlayMusic();
+        // sp.PlayMusic();
+        // sp.TakePhoto();
 
-        // for (Gadgets fun : functions) {
-        // fun.TakePhoto();
-        // fun.PlayMusic();
-        // }
+        TakePhoto[] photoDevice = {
+                new Camera(), new smartPhone()
+        };
+
+        PlayMusic[] musicDevice = {
+                new MP3Player(), new smartPhone()
+        };
+
+        for (TakePhoto tp : photoDevice) {
+            tp.TakePhoto();
+        }
+        for (PlayMusic pm : musicDevice) {
+            pm.PlayMusic();
+        }
     }
 }
