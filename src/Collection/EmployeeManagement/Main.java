46337ebf1dd60e2ca854376c948ac26216600204
Vishal <vnaik9900@gmail.com>
2025-10-12 16:57:43 +0530

    Collection Emp management updated

--- src/Collection/EmployeeManagement/Main.java
+++ src/Collection/EmployeeManagement/Main.java
@@ -2,11 +2,28 @@ package Collection.EmployeeManagement;
 
 public class Main {
     public static void main(String[] args) {
-        Employee[] employees = { new Employee(101, "Vishal", 20000),
-                new Employee(102, "Vishal Naik", 100000) };
+        // Employee[] employees = { new Employee(101, "Vishal", 20000),
+        // new Employee(102, "Vishal Naik", 100000) };
 
-        for (Employee emp : employees) {
-            emp.displayEmployeeDetails();
+        // for (Employee emp : employees) {
+        // emp.displayEmployeeDetails();
+        // }
+
+        PayrollSystem empPay = new PayrollSystem();
+
+        empPay.addEmployee(new Employee(101, "Vishal", 10000));
+        empPay.addEmployee(new Employee(102, "VishalS", 120000));
+        empPay.addEmployee(new Employee(103, "VishalSNaik", 130000));
+        empPay.addEmployee(new Employee(104, "VishalShridharNaik", 140000));
+
+        Employee e = empPay.searchEmployee(105);
+        if (e != null) {
+            System.out.println("\nFound Employee");
+            e.displayEmployeeDetails();
+        } else {
+            System.out.println("Employee not present with the mentioned Id");
         }
+
+        empPay.displayAll();
     }
 }
--- src/Collection/EmployeeManagement/PayrollSystem.java
+++ src/Collection/EmployeeManagement/PayrollSystem.java
@@ -1,5 +1,31 @@
 package Collection.EmployeeManagement;
 
+import java.util.ArrayList;
+
 public class PayrollSystem {
+    ArrayList<Employee> empList = new ArrayList<>();
+
+    public void addEmployee(Employee e) {
+        empList.add(e);
+    }
+
+    public Employee searchEmployee(int id) {
+        for (Employee emp : empList) {
+            if (emp.getEmpId() == id) {
+                return emp;
+            }
+        }
+        return null;
+    }
+
+    public void displayAll() {
+        if (empList.isEmpty()) {
+            System.out.println("No Employees are available");
+        } else {
+            for (Employee emp : empList) {
+                emp.displayEmployeeDetails();
+            }
+        }
+    }
 
 }
