1a75f6f89405d2470f75aa6946a7fba89d42ae95
Vishal <vnaik9900@gmail.com>
2025-10-12 16:11:39 +0530

    Collection emp management update

--- src/Collection/EmployeeManagement/Employee.java
+++ src/Collection/EmployeeManagement/Employee.java
@@ -1,5 +1,25 @@
 package Collection.EmployeeManagement;
 
 public class Employee {
+    private int empId;
+    protected String name;
+    private double salary;
 
+    Employee(int empId, String name, double salary) {
+        this.empId = empId;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void displayEmployeeDetails() {
+        System.out.println("Employee Id: " + empId + " Employee Name: " + name + " Employee Salary " + salary);
+    }
 }
--- src/Collection/EmployeeManagement/Main.java
+++ src/Collection/EmployeeManagement/Main.java
@@ -1,5 +1,12 @@
 package Collection.EmployeeManagement;
 
 public class Main {
+    public static void main(String[] args) {
+        Employee[] employees = { new Employee(101, "Vishal", 20000),
+                new Employee(102, "Vishal Naik", 100000) };
 
+        for (Employee emp : employees) {
+            emp.displayEmployeeDetails();
+        }
+    }
 }
