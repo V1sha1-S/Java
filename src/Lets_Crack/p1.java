d02ed3a99dae989d0c4b99c0b8a68ff32075e6aa
Vishal <vnaik9900@gmail.com>
2026-02-05 01:03:05 +0530

    Assignment 0

---
+++ src/Lets_Crack/p1.java
@@ -0,0 +1,19 @@
+package Lets_Crack;
+
+import java.util.Scanner;
+
+public class p1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+        double balance = sc.nextDouble();
+
+        if (age >= 18 && balance >= 1000) {
+            System.out.println("Age is above 18 or above and balance is greater than or equal to 1000");
+        } else {
+            System.out.println("age is less than 18 and balance is less than 1000");
+        }
+    }
+
+}
