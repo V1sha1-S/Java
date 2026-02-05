b5612bdc389c50bfdfdbf777875dcf39aa22cb0f
Vishal <vnaik9900@gmail.com>
2026-02-06 00:41:55 +0530

    Assignment 1

---
+++ src/Lets_Crack/Assignment1.java
@@ -0,0 +1,66 @@
+package Lets_Crack;
+
+import java.util.Scanner;
+
+class Account {
+    private double balance;
+    private int AccountNumber;
+
+    Account(double balance, int AccountNumber) {
+        this.balance = balance;
+        this.AccountNumber = AccountNumber;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Enter a valid amount");
+        } else {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double withdrawAmount) {
+        if (withdrawAmount <= 0) {
+            System.out.println("Enter a valid amount");
+        } else {
+            balance -= withdrawAmount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setAccountNumber(int accNo) {
+        AccountNumber = accNo;
+    }
+
+    public int getAccountNumber() {
+        return AccountNumber;
+    }
+
+}
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the amount");
+        double amount = sc.nextDouble();
+        System.out.println("Enter the Account Number");
+        int accNo = sc.nextInt();
+        System.out.println("Enter the amount to be withdraw");
+        double withdrawAmount = sc.nextDouble();
+
+        Account account = new Account(0, accNo);
+        account.deposit(amount);
+        System.out.println(
+                "Amount deposited to the " + account.getAccountNumber() + " total balance: " + account.getBalance());
+
+        System.out.println("Total balance after deposit: " + account.getBalance());
+
+        account.withdraw(withdrawAmount);
+        System.out.println("Balance after withdrawal: " + account.getBalance());
+
+    }
+}
--- src/Lets_Crack/p1.java
+++ src/Lets_Crack/p1.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class p1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int age = sc.nextInt();
         double balance = sc.nextDouble();
-
         if (age >= 18 && balance >= 1000) {
             System.out.println("Age is above 18 or above and balance is greater than or equal to 1000");
         } else {
