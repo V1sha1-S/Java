3d3a25a9685c728f5442b956aa15c99c4b05f2d0
Vishal <vnaik9900@gmail.com>
2026-03-09 23:25:14 +0530

    Pyramid patter

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.err.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
     }
 }
