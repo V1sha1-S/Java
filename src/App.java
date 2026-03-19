a0aa0795e26b54283dd3bec11c8ab495653f5d63
Vishal <vnaik9900@gmail.com>
2026-03-19 20:05:58 +0530

    Pattern 1.1

--- src/App.java
+++ src/App.java
@@ -39,13 +39,19 @@ public class App {
         // System.out.println();
         // }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i; j++) {
-                System.out.print("*");
+        // for (int i = 0; i < n; i++) {
+        // for (int j = 0; j < n - i; j++) {
+        // System.out.print("*");
+        // }
+
+        // System.out.println();
+        // }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
             }
-            // for (int j = 0; j < n - i; j++) {
-            // System.out.print("*");
-            // }
+
             System.out.println();
         }
 
