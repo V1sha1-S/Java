82270a63e948c98c7fba604a529e045e727f3f96
Vishal <vnaik9900@gmail.com>
2026-03-19 19:56:02 +0530

    Pattern 1

--- src/App.java
+++ src/App.java
@@ -4,13 +4,48 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
+        // Pyramid
+        // for (int i = 0; i < n; i++) {
+        // for (int j = 0; j < n - i; j++) {
+        // System.err.print(" ");
+        // }
+        // for (int j = 0; j < i + 1; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // Square
+        // for (int i = 0; i < n; i++) {
+        // for (int j = 0; j < n; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // right angle
+        // for (int i = 0; i < n; i++) {
+        // for (int j = 0; j < i + 1; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // for (int i = 0; i < n; i++) {
+        // for (int j = 0; j < n - i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - i; j++) {
-                System.err.print(" ");
+                System.out.print("*");
             }
-            for (int j = 0; j < i + 1; j++) {
-                System.out.print("* ");
-            }
+            // for (int j = 0; j < n - i; j++) {
+            // System.out.print("*");
+            // }
             System.out.println();
         }
 
